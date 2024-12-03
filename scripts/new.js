98f918ef6c37d3b851fcd56a0832e56790ee642c
Paul Javaloyes <pjavaloy@emeal.nttdata.com>
2024-12-03 14:35:15 +0100

    format

--- scripts/new.js
+++ scripts/new.js
@@ -1,27 +1,27 @@
-import * as path from "jsr:@std/path";
+import * as path from 'jsr:@std/path'
 
-const { join, dirname } = path;
-const projectDirectory = path.resolve('.');
-const src = join(projectDirectory, 'src');
-const tests = join(projectDirectory, 'tests');
+const { join, dirname } = path
+const projectDirectory = path.resolve('.')
+const src = join(projectDirectory, 'src')
+const tests = join(projectDirectory, 'tests')
 
-const [day] = Deno.args;
-const newDayRaw = day ?? (await getDay(src));
-const newDay = `day_${newDayRaw.toString().padStart(2, '0')}`;
-console.log(`Creating new day: ${newDay}`);
+const [day] = Deno.args
+const newDayRaw = day ?? (await getDay(src))
+const newDay = `day_${newDayRaw.toString().padStart(2, '0')}`
+console.log(`Creating new day: ${newDay}`)
 
-await createSrcFile(src, newDay, newDayRaw);
-await createTestFile(tests, newDay, newDayRaw);
+await createSrcFile(src, newDay, newDayRaw)
+await createTestFile(tests, newDay, newDayRaw)
 
 async function createSrcFile(src, day, dayRaw) {
-    const FILE_NAME = 'index.js';
-    const README = 'README.md';
+  const FILE_NAME = 'index.js'
+  const README = 'README.md'
 
-    const nextDaySrcPath = join(src, day);
-    await Deno.mkdir(nextDaySrcPath);
+  const nextDaySrcPath = join(src, day)
+  await Deno.mkdir(nextDaySrcPath)
 
-    const solutionPath = join(nextDaySrcPath, FILE_NAME);
-    const solutionContent = `export const solvePartOne = (input) => {
+  const solutionPath = join(nextDaySrcPath, FILE_NAME)
+  const solutionContent = `export const solvePartOne = (input) => {
   return input;
 }
 
@@ -35,22 +35,22 @@ export const main = async () => {
   console.log('Part two solution:', solvePartTwo(input))
 }
 `
-    await Deno.writeTextFile(solutionPath, solutionContent);
+  await Deno.writeTextFile(solutionPath, solutionContent)
 
-    const readmePath = join(nextDaySrcPath, README);
+  const readmePath = join(nextDaySrcPath, README)
 
-    const response = await fetch(`https://adventofcode.com/2024/day/${newDayRaw}`)
-    const text = await response.text();
-    const [readmeContent] = text.match(/<main\b[^>]*>([\s\S]*?)<\/main>/);
+  const response = await fetch(`https://adventofcode.com/2024/day/${newDayRaw}`)
+  const text = await response.text()
+  const [readmeContent] = text.match(/<main\b[^>]*>([\s\S]*?)<\/main>/)
 
-    await Deno.writeTextFile(readmePath, readmeContent);
+  await Deno.writeTextFile(readmePath, readmeContent)
 }
 
 async function createTestFile(tests, day, dayRaw) {
-    const FILE_NAME = `${day}.test.js`;
+  const FILE_NAME = `${day}.test.js`
 
-    const nextDayTestPath = join(tests, FILE_NAME);
-    const testContent = `
+  const nextDayTestPath = join(tests, FILE_NAME)
+  const testContent = `
 import { solvePartOne, solvePartTwo } from '../src/${day}/index.js'
 import { assertEquals } from 'jsr:@std/assert'
 
@@ -76,16 +76,16 @@ Deno.test({
   },
 })
 `
-    await Deno.writeTextFile(nextDayTestPath, testContent);
+  await Deno.writeTextFile(nextDayTestPath, testContent)
 }
 
 async function getDay(src) {
-    const days = [];
-    for await (const { isDirectory, name } of Deno.readDir(src)) {
-        if (!isDirectory) continue;
-        days.push(name);
-    }
+  const days = []
+  for await (const { isDirectory, name } of Deno.readDir(src)) {
+    if (!isDirectory) continue
+    days.push(name)
+  }
 
-    const [lastDay] = days.at(-1).match(/\d+/);
-    return +lastDay + 1;
+  const [lastDay] = days.at(-1).match(/\d+/)
+  return +lastDay + 1
 }
